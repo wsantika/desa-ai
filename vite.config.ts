72ad0c932f8f056e71f4d8a5732e9f5be583453f
wsantika <wahyuputra7396@gmail.com>
2026-09-18 17:59:33 +0800

    chore(vite): configure host 0.0.0.0 and port 3000 for docker support

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
 })
 
