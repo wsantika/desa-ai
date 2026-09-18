e4a228db4656dc8a84ce071c98fc86c89f92736d
wsantika <wahyuputra7396@gmail.com>
2026-09-18 17:56:31 +0800

    chore(git): add commitlint configuration for conventional commits

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'ci',
+        'revert',
+      ],
+    ],
+    'subject-case': [2, 'never', ['upper-case', 'pascal-case', 'start-case']],
+  },
+}
